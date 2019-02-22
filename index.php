e1b40402575be25cbae7fa9db039beb9cac07145
faniemanaswe <44095979+faniemanaswe@users.noreply.github.com>
2019-02-22 04:21:58 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Portfolio</title>
+	<meta charset="utf-8" />
+</head>
+<body>
+<?php
+include("menu.inc");
+ 
+echo "<h3>Name: Fanie Manaswe</h3>";
+echo "<h3>Student Number: 54384508</h3>";
+echo "<h3>Semester 2 Portfolio 2018</h3>";
+ 
+ 
+?>
+</body>
+</html>
